afd9e068263092ba645796d8385307598e1e4009
bc3 <ben_croughs@hotmail.com>
2014-04-18 13:20:21 +0200

    ASMX handler

---
+++ GoogleAnalyticsTracker.ASMX/CurrentRequestAnalyticsSession.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using GoogleAnalyticsTracker.Core;
+
+namespace GoogleAnalyticsTracker.ASMX
+{
+    public class CurrentRequestAnalyticsSession : AnalyticsSession, IAnalyticsSession
+    {
+
+        protected HttpContextBase GetHttpContext()
+        {
+            if (HttpContext.Current != null)
+            {
+                return new HttpContextWrapper(HttpContext.Current);
+            }
+            return null;
+        }
+
+        protected override string GetUniqueVisitorId()
+        {
+            string cookie;
+            if (GetHttpContext() != null && GetHttpContext().Request.Cookies["__utma"] != null && string.IsNullOrEmpty(GetHttpContext().Request.Cookies["__utma"].Value))
+            {
+                cookie = base.GetUniqueVisitorId();
+            }
+            else
+            {
+                cookie = GetHttpContext().Request.Cookies["__utma"].Value.Split('.')[1];
+            }
+            return cookie;
+        }
+
+        protected override int GetFirstVisitTime()
+        {
+            int cookie;
+            if (GetHttpContext() != null && GetHttpContext().Request.Cookies["__utma"] != null && string.IsNullOrEmpty(GetHttpContext().Request.Cookies["__utma"].Value))
+            {
+                cookie = base.GetFirstVisitTime();
+            }
+            else
+            {
+                cookie = Convert.ToInt32(GetHttpContext().Request.Cookies["__utma"].Value.Split('.')[2]);
+            }
+            return cookie;
+        }
+    }
+}
